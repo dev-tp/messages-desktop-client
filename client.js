6a26a3df9f9a843d3625ba71d0dcd13bc64f899e
Eryk <rcksorto@yahoo.com>
2017-12-21 00:00:00 -0800

    Format date from timestamp

--- client.js
+++ client.js
@@ -1,9 +1,39 @@
 const net = require('net');
 
 const client = new net.Socket();
+const months = [
+  'Jan', 'Feb', 'Mar', 'Apr',
+  'May', 'Jun', 'Jul', 'Aug',
+  'Sep', 'Oct', 'Nov', 'Dec'
+];
 
 let database = null;
 let segments = [];
+
+function getReadableDate(currentDate, timestamp) {
+  const date = new Date(timestamp);
+
+  const currentDay = currentDate.getDate();
+  const day = date.getDate();
+  const month = date.getMonth();
+  const year = date.getFullYear();
+
+  let hours = date.getHours();
+  hours = hours < 10 ? '0' + hours : '' + hours;
+
+  let minutes = date.getMinutes();
+  minutes = minutes < 10 ? '0' + minutes : '' + minutes;
+
+  const time = hours + ':' + minutes;
+
+  if (currentDate.getFullYear() > year) {
+    return months[month] + ' ' + day + ', ' + year;
+  } else if (currentDay < day || day < currentDay) {
+    return months[month] + ' ' + day + ' ' + time;
+  }
+
+  return time;
+}
 
 client.connect(1337, '127.0.0.1', function () {
   console.log('Client connected to server');
@@ -19,6 +49,7 @@ client.on('data', function (data) {
       database = JSON.parse(segments.join(''));
       segments = [];
 
+      const date = new Date();
       const threadsDom = document.getElementById('threads');
 
       Object.keys(database).forEach(function (key) {
@@ -39,7 +70,7 @@ client.on('data', function (data) {
 
         const dateDom = document.createElement('p');
         dateDom.classList.add('date');
-        dateDom.innerText = lastMessage.date;
+        dateDom.innerText = getReadableDate(date, lastMessage.date);
 
         threadDom.date = lastMessage.date;
 
